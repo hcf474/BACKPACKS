8d7b4bf91c7f4bceff313894adda9122eae9f4a6
hcf474 <camarillohector222@gmail.com>
2026-06-08 19:15:27 -0600

    Update car.js

--- car.js
+++ car.js
@@ -1,3 +1,17 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD7mfb7qmKhUTskFaOu4Fxc4KFSnccsNuA",
+    authDomain: "backpack-4eec7.firebaseapp.com",
+    projectId: "backpack-4eec7",
+    storageBucket: "backpack-4eec7.firebasestorage.app",
+    messagingSenderId: "690480159566",
+    appId: "1:690480159566:web:90a46f81eb7548c03f1c1f",
+    measurementId: "G-577JY6EV8B"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+const db = firebase.firestore();
 const getCart = () => JSON.parse(localStorage.getItem('carrusel_cart')) || [];
 const saveCart = (cart) => localStorage.setItem('carrusel_cart', JSON.stringify(cart));
 
